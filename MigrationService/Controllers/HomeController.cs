d9272b930289b241a5dbb4bf2136d01e92138e41
Nikita Zhuklin <nikita-zhuklin@rambler.ru>
2025-04-20 19:54:21 +0400

    add main page

---
+++ MigrationService/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using MigrationService.Models;
+
+namespace MigrationService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MigrationDbContext _context;
+
+        public HomeController(MigrationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Get counts for dashboard
+            ViewBag.MigrantsCount = _context.Migrants.Count();
+            ViewBag.OfficersCount = _context.Officers.Count();
+            ViewBag.ApplicationsCount = _context.Applications.Count();
+            ViewBag.CountriesCount = _context.Countries.Count();
+            ViewBag.LanguagesCount = _context.Languages.Count();
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+} 
